414d918f197ed1820566b8b37be7821380c18cbd
rallan123 <rasmus.jonasson91@gmail.com>
2016-04-04 15:20:16 +0200

    Added skeleton for TypeScriptEducation

---
+++ app.ts
@@ -0,0 +1,28 @@
+//Step 4 - Get textfield and button from markup
+
+//Step 5 - Add _empty_ button click event
+//Step 10 - Add todo item to existing list
+//Step 14 - Optional - Create instance of new class
+//Step 16 - Optional - Add generics
+
+
+//Step 6 -- Get list container from markup
+
+
+//Step 13 - Optional - Create Todo class
+//Step 17 - Optional - Add generics
+
+
+//Step 7 - JavaScript array to contain all todo items
+//Step 12 - Optional - Modify array to contain implementation of new interface
+//Step 18 - Optional - Add generics
+
+
+//Step 8 - Create function to add element to container from "Step 5"
+
+
+//Step 9 - Loop through all todo items and add to container from "Step 5"
+
+
+//Step 11 - Optional - Create interface
+//Step 15 - Optional - Modify to be generic
